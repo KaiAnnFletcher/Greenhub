a2fa11d725b7414cb96e567e5d7f16865455c815
Kai <kaiann.fletcher@mail.utoronto.ca>
2021-12-14 22:01:42 -0500

    Uncommnented routes for React app for now (index).

---
+++ client/build/precache-manifest.d488001871087f6af5aec170236d486e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09a31d4f3cf0975ed945cdf3db13dc14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8c6ebd9d540be262365",
+    "url": "/static/css/main.cbc935a0.chunk.css"
+  },
+  {
+    "revision": "f2ef07f0bb7d525b8dee",
+    "url": "/static/js/2.f6425878.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/static/js/2.f6425878.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8c6ebd9d540be262365",
+    "url": "/static/js/main.12f6beb7.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
